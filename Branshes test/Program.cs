6d2e1a3b6faaf2de65b0e0f4e3c457198b64a466
snillet1992 <johan.p.sellstrom@gmail.com>
2022-11-29 09:35:00 +0100

    new spaceship

--- Branshes test/Program.cs
+++ Branshes test/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var spaceship = new Spaceship() { Name = "Millenium Falcon", Description = "Super fast"};
         }
     }
 }
