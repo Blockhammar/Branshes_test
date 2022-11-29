a18408cee0fd576a1f06b115421dadbef3f8be6c
Joakim Lignerkrona <jockan@icloud.com>
2022-11-29 09:21:49 +0100

    added alien

---
+++ Branshes test/Alien.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branshes_test
+{
+    internal class Alien
+    {
+        string Name { get; set; }
+        string Race { get; set; }
+    }
+}
